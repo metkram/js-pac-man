b009fc137435af80946a485611ffce0757f985de
Andwer T <metkraming@gmail.com>
2021-11-06 23:07:06 +0200

    add ability to change pac-man direction

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ class Game {
     this.pacMan = new PacMan();
     this.timer;
   }
-  render({field, pacManY, pacManX, pacManDirY, pacManDirX, nextMove}) {
+  render({field, pacManY, pacManX, prevPacManY, prevPacManX, pacManDirY, pacManDirX, nextMove}) {
     console.clear();
     console.log(field[pacManY - pacManDirY][pacManX - pacManDirX]);
-    field[pacManY - pacManDirY][pacManX - pacManDirX] = "_"; //I have to fix borber render
+    console.log(this.pacMan.nextMove);
+    console.log(this.pacMan.prevCoordinates);
+    console.log(this.pacMan.currentCoordinates);
+    field[prevPacManY][prevPacManX] = "_"; //I have to fix it, it replaces one step away field
     field[pacManY][pacManX] = PacMan.image[nextMove];
     for (let line of field) {
       console.log(line.join());
@@ -21,19 +24,40 @@ class Game {
     if (field[pacManY + pacManDirY][pacManX + pacManDirX] == "#") {
       this.stop();
     } else {
-      this.pacMan.coordinates[0] += pacManDirY;
-      this.pacMan.coordinates[1] += pacManDirX;
+      this.pacMan.prevCoordinates[0] = this.pacMan.currentCoordinates[0];
+      this.pacMan.prevCoordinates[1] = this.pacMan.currentCoordinates[1];
+      this.pacMan.currentCoordinates[0] += pacManDirY;
+      this.pacMan.currentCoordinates[1] += pacManDirX;
     }
   }
 
   start() {
     console.log("hi there, soon I'll be the Pac-Man game");
     let t = this;
+    let changeDirection = function(event) {
+      switch (event.key) {
+        case "ArrowUp":
+          t.pacMan.nextMove = "up";
+          break;
+        case "ArrowDown":
+          t.pacMan.nextMove = "down";
+          break;
+        case "ArrowLeft":
+          t.pacMan.nextMove = "left";
+          break;
+        case "ArrowRight":
+          t.pacMan.nextMove = "right";
+          break;
+      }
+    }
+    document.addEventListener("keydown", changeDirection);
     setTimeout(function run() {
       let options = {
         field: t.field.grid,
-        pacManY: t.pacMan.coordinates[0],
-        pacManX: t.pacMan.coordinates[1],
+        pacManY: t.pacMan.currentCoordinates[0],
+        pacManX: t.pacMan.currentCoordinates[1],
+        prevPacManY: t.pacMan.prevCoordinates[0],
+        prevPacManX: t.pacMan.prevCoordinates[1],
         pacManDirY: PacMan.direction[t.pacMan.nextMove][0],
         pacManDirX: PacMan.direction[t.pacMan.nextMove][1],
         nextMove: t.pacMan.nextMove,
--- pacMan.js
+++ pacMan.js
@@ -6,13 +6,14 @@ class PacMan {
     "right": "<",
   };
   static direction = {
-    "up": [1, 0],
-    "down": [-1, 0],
+    "up": [-1, 0],
+    "down": [1, 0],
     "left": [0, -1],
     "right": [0, 1],
   }
   constructor() {
-    this.coordinates = [2, 1];
+    this.prevCoordinates = [2, 1];
+    this.currentCoordinates = [2, 1];
     this.nextMove = "right";
   }
 }
